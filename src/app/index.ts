017e3e4ddbfecf116d22ced086a422e8bcfdbd50
dengnh <dengninghai@shinemo.com>
2022-07-20 16:55:02 +0800

    feat：完善文件上传接口

--- src/app/index.ts
+++ src/app/index.ts
@@ -14,10 +14,15 @@ connectMongodb();
 // 注册解析参数的中间件
 app.use(
   koaBody({
+    // // 支持文件上传
     multipart: true,
     formidable: {
+      // 图片保存的静态资源文件路径
       uploadDir: path.join(__dirname, "../upload"),
+      // 是否保留扩展名
       keepExtensions: true,
+      // 文件上传大小
+      maxFieldsSize: 20 * 1024 * 1024,
     },
   })
 );
--- src/controller/upload.controller.ts
+++ src/controller/upload.controller.ts
@@ -5,15 +5,13 @@ class UploadController {
   // 创建文章
   async uploadFileCtr(ctx, next) {
     const { file } = ctx.request.files;
-    console.log(file, "file");
     if (file) {
       const basename = path.basename(file.filepath);
-      console.log(basename, "basename");
       ctx.body = {
         code: 200,
         message: "文件上传成功",
         data: {
-          filePath: `${ctx.origin}/${basename}`, //"url": `${ctx.origin}/static/${basename}`
+          filePath: `${ctx.origin}/${basename}`,
         },
       };
     } else {
