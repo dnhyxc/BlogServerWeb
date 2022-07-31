760093576e60413034e24b651362a62060a8e3fb
dengnh <dengninghai@shinemo.com>
2022-07-31 16:07:08 +0800

    feat：搜索去除登录校验

--- src/router/article.router.ts
+++ src/router/article.router.ts
@@ -18,19 +18,19 @@ router.post("/createArticle", auth, createArticleCtr);
 // 更新文章
 router.post("/updateArticle", auth, updateArticleCtr);
 
-// 更新文章
-router.post("/searchArticle", auth, searchArticleCtr);
-
 // 删除文章
 router.post("/deleteArticle", auth, deleteArticleCtr);
+
+// 文章点赞
+router.post("/likeArticle", auth, likeArticleCtr);
 
 // 获取文章
 router.post("/articleList", getArticleListCtr);
 
+// 搜索文章
+router.post("/searchArticle", searchArticleCtr);
+
 // 获取文章详情
 router.post("/articleDetail", getArticleByIdCtr);
-
-// 文章点赞
-router.post("/likeArticle", likeArticleCtr);
 
 module.exports = router;
